194fa2cf75679bd684b60bd0ea398b5d2b41027b
Kenneth Coleman <200403248@student.georgianc.on.ca>
2020-01-23 20:49:46 -0500

    completed lab3

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        List<Phone> PhoneList = new List<Phone>
+        {
+            new Phone {PhoneID = 1, PhoneName = "S10", Manufacturer = "Samsung", Msrp = 500.00, ScreenSize = "6\"", DateReleased = DateTime.Parse("2019-01-01")},
+            new Phone {PhoneID = 2, PhoneName = "iPhone 11", Manufacturer = "Apple", Msrp = 1000.00, ScreenSize = "6.5\"", DateReleased = DateTime.Parse("2019-12-02")},
+            new Phone {PhoneID = 2, PhoneName = "Pixel", Manufacturer = "Google", Msrp = 1200.00, ScreenSize = "7\"", DateReleased = DateTime.Parse("2018-11-15")}
+        };
+
+        // GET: Phone
+        public ActionResult Index()
+        {
+            return View(PhoneList);
+        }
+    }
+}
---
+++ Models/Phone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone { 
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public double Msrp { get; set; }
+        public string ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+        public Phone()
+        {
+            PhoneName = "";
+            Manufacturer = "";
+            ScreenSize = "";
+            DateReleased = DateTime.Parse("2020-01-01");
+        }
+    }
+}
